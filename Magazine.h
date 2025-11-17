2fa8218286e693ee4a8246e59d52b2330bcd5ec0
RylandLoyd <rylandjloyd@gmail.com>
2025-11-17 11:53:07 -0800

    Add Magazine class definition in Magazine.h

--- Magazine.h
+++ Magazine.h
@@ -1,1 +1,18 @@
 
+
+#include "Item.h"
+
+#ifndef MAGAZINE_H
+#define MAGAZINE_H
+
+class Magazine : public Item
+{
+private:
+	string edition;
+	string mainArticle;
+public:
+	Magazine(const string& name, const string& description, int id, const string& edition, const string& mainArticle);
+
+	void print(ostream& os) const override;
+};
+#endif
