df50d8919adfb0d56dbcbd8847a2446db688560f
RylandLoyd <rylandjloyd@gmail.com>
2025-11-17 11:53:25 -0800

    Implement Movie class with title and director attributes
    Added Movie class that inherits from Item with attributes for title, director, and actors.

--- Movie.h
+++ Movie.h
@@ -1,1 +1,20 @@
+#include "Item.h"
 
+#ifndef MOVIE_H
+#define MOVIE_H
+
+class Movie : public Item
+{
+private:
+	string title;
+	string director;
+	vector<string> actors;
+
+public:
+	Movie(const string& name, const string& description, int id, const string& title, const string& director, const vector<string>& actors);
+
+	void addActor(const string& actor);
+
+	void print(ostream& os) const;
+};
+#endif
