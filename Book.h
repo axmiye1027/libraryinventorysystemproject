88db9ac1db2956850738b51c429886cc3bba2945
RylandLoyd <rylandjloyd@gmail.com>
2025-11-17 11:52:15 -0800

    Define Book class inheriting from Item

--- Book.h
+++ Book.h
@@ -1,1 +1,18 @@
 
+#include "Item.h"
+
+#ifndef BOOK_H
+#define BOOK_H
+
+class Book : public Item
+{
+private:
+	string title;
+	string author;
+	string copyrightDate;
+public:
+	Book(const string& name, const string& description, int id, const string& title, const string& author, const string& copyrightDate);
+
+	void print(ostream& os) const override;
+};
+#endif
